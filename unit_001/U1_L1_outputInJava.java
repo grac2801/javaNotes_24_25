8e5000a8538b818afbbd96e3a38721320d16c969
grac2801 <oscar.gracias@icloud.com>
2024-07-26 14:07:49 -0700

    U1_L1 output in java

---
+++ unit_001/U1_L1_outputInJava.java
@@ -0,0 +1,24 @@
+package unit_001;
+
+public class U1_L1_outputInJava
+{
+
+	public static void main(String[] args)
+	{
+		System.out.print("Today is");
+		System.out.println(" Friday");
+		System.out.println("Hello World");
+		
+		System.out.println("76575");
+		System.out.println("*&^%");
+		
+		System.out.println("1 + 5");
+		System.out.println("The total is: " + 1 + 5);
+		
+		System.out.println("The total is: " + (1 + 5));
+		
+		
+		
+	}
+
+}
