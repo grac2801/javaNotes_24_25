4cd65c320ca4652486e516cb51ad7f5b7d599ea9
grac2801 <oscar.gracias@icloud.com>
2024-08-23 14:17:31 -0700

    will continue on Tuesday. Started Math library

---
+++ unit_002/U2_L8_MathClass.java
@@ -0,0 +1,74 @@
+package unit_002;
+
+import java.util.Scanner;
+
+public class U2_L8_MathClass
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println(Math.abs(5));
+		System.out.println(Math.abs(-5.4));
+		System.out.println(Math.pow(2, 3));
+		System.out.println((int) Math.pow(2, 3));
+		System.out.println(Math.sqrt(8.8));
+		System.out.printf("$%.2f%n", Math.sqrt(8.8));
+		
+		
+		//Create a scanner
+		Scanner scan = new Scanner(System.in);
+//		System.out.println("Enter 2 decimal numbers: ");
+//		double n1 = scan.nextDouble();
+//		double n2 = scan.nextDouble();
+		
+//		System.out.println("n1 to the power of n2: " + Math.pow(n1, n2));
+		
+		//non-Static
+		String word = new String("apple");
+		System.out.println("The length of word is: " + word.length());
+		
+		
+		//Static means it belongs to the class, and you need to ask permission
+		//by calling the class first.
+		
+		System.out.println(Math.random()); //values between 0 and 1 (1 not included)
+		
+		//A random integer number between 10 and 15
+		int myRandomNumber =(int) (Math.random() * 6) + 10;
+		System.out.println(myRandomNumber);
+		
+		//Get a random number from 1 to 100 included
+		System.out.println((int)(Math.random() * 100) + 1);
+		
+		
+		//10 to 34 integers only
+		System.out.println((int) (Math.random() * 25) + 10);
+		
+		
+		
+		//-10 to 10 integers only
+		System.out.println((int) ((Math.random() * 21) + 10));
+		
+		//All the even integers from 1 - 100
+		int evens = ((int)(Math.random() * 50) + 1) * 2;
+		System.out.println(evens);
+		
+		
+		
+		// all the odd numbers from 1 to 100
+		int odds = ((int)(Math.random() * 50) * 2) + 1;
+		System.out.println(odds);
+		
+		
+		
+		
+		
+		
+		
+//		scan.close();
+		
+		
+		
+	}
+
+}
