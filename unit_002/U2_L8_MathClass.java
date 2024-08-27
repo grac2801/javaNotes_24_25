19903294c4e542924e4057162da6bd992a6e1631
grac2801 <oscar.gracias@icloud.com>
2024-08-27 13:58:00 -0700

    completed randomizers

--- unit_002/U2_L8_MathClass.java
+++ unit_002/U2_L8_MathClass.java
@@ -1,6 +1,8 @@
 package unit_002;
 
+import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class U2_L8_MathClass
 {
@@ -59,6 +61,28 @@ public class U2_L8_MathClass
 		int odds = ((int)(Math.random() * 50) * 2) + 1;
 		System.out.println(odds);
 		
+		//All numbers 1 -100 divisible by 5
+		int divisibleBy5 = (int)(Math.random() * 20 + 1) * 5;
+		System.out.println("divisible by 5: " + divisibleBy5);
+		
+		//All numbers 0-25 multiples of 3
+		int divisibleBy3 = (int)(Math.random() * 8 + 1) * 3;
+		System.out.println("divisible by 5: " + divisibleBy3);
+		
+		
+		System.out.println("********************");
+		System.out.println("#2 class Random");
+		System.out.println("********************");
+		Random rand = new Random();
+		int myRandomValue = rand.nextInt(10) + 1;
+		System.out.println("The Random value is: " + myRandomValue);
+		
+		System.out.println("********************");
+		System.out.println("#3 ThreadLocalRandom class");
+		System.out.println("********************");
+		//Select a random day in a month of 31 days
+		int day = ThreadLocalRandom.current().nextInt(31) + 1;
+		System.out.println("The day of the month is: " + day);
 		
 		
 		
